65d36daa9503b34bf65b7ef9c74df4fca2e8a8f5
Patricia Montag <post@patricia-montag.de>
2020-02-19 17:19:51 +0100

    extend data quality operator to check for null values

--- airflow/plugins/operators/data_quality.py
+++ airflow/plugins/operators/data_quality.py
@@ -7,6 +7,19 @@ class DataQualityOperator(BaseOperator):
     
     ui_color = '#89DA59'
 
+    information_sql = """
+            SELECT column_name
+            FROM information_schema.columns
+            WHERE table_name = '{}'
+            AND is_nullable = 'NO'
+        """
+
+    null_sql = """
+            SELECT COUNT(*)
+            FROM {}
+            WHERE {} is null
+    """
+
     @apply_defaults
     def __init__(self,
                  tables,
@@ -40,5 +53,29 @@ class DataQualityOperator(BaseOperator):
 
         if not checks_passed:
             raise ValueError(f'Data quality check failed for Tables {affected_tables}')
-               
+
+        null_checks_passed = True
+        null_affected_tables = []
+        # null checks for each given table
+        for table in tables:
+            formatted_information_sql = DataQualityOperator.information_sql.format(table)
+            self.log.info(f'SQL to be executed: {formatted_information_sql}')
+            records = redshift.get_records(formatted_information_sql)
+            self.log.info(f'records {records}, len: {len(records)}')
+
+            x = len(records)
+            if x > 0:
+                for i in range(x):
+                    formatted_null_sql = DataQualityOperator.null_sql.format(table, records[0][0])
+                    self.log.info(f'SQL to be executed: {formatted_null_sql}')
+                    null_records = redshift.get_records(formatted_null_sql)
+                    num_null_records = null_records[0][0]
+
+                    if num_null_records > 0:
+                        null_checks_passed = False
+                        null_affected_tables.append(table)
+
+        if not null_checks_passed:
+            raise ValueError(f'Data quality NULL check failed for Tables {null_affected_tables}')
+
         self.log.info(f'Data quality checks have been successful for {self.tables}')
